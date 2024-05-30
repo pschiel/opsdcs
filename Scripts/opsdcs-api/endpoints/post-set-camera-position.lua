fa18f7b1999781ab5a70cd63a37b42c488987cb9
Patrick Schiel <p.schiel@gmail.com>
2024-05-30 05:52:56 +0200

    cleanup camera functions

--- Scripts/opsdcs-api/endpoints/post-set-camera-position.lua
+++ Scripts/opsdcs-api/endpoints/post-set-camera-position.lua
@@ -1,17 +1,12 @@
 local function applyRotation(a, b, angle)
+    local cos_angle, sin_angle = math.cos(angle), math.sin(angle)
     local ax, ay, az, bx, by, bz = a.x, a.y, a.z, b.x, b.y, b.z
-    a.x = math.cos(angle) * ax + math.sin(angle) * bx
-    a.y = math.cos(angle) * ay + math.sin(angle) * by
-    a.z = math.cos(angle) * az + math.sin(angle) * bz
-    b.x = math.cos(angle) * bx - math.sin(angle) * ax
-    b.y = math.cos(angle) * by - math.sin(angle) * ay
-    b.z = math.cos(angle) * bz - math.sin(angle) * az
+    a.x, a.y, a.z = cos_angle * ax + sin_angle * bx, cos_angle * ay + sin_angle * by, cos_angle * az + sin_angle * bz
+    b.x, b.y, b.z = cos_angle * bx - sin_angle * ax, cos_angle * by - sin_angle * ay, cos_angle * bz - sin_angle * az
 end
 
 local function getOrientation(roll, pitch, heading)
-    local h = math.rad(heading)
-    local p = math.rad(pitch)
-    local r = math.rad(roll)
+    local h, p, r = math.rad(heading), math.rad(pitch), math.rad(roll)
     local o = {x = {x = 1, y = 0, z = 0}, y = {x = 0, y = 1, z = 0}, z = {x = 0, y = 0, z = 1}}
     applyRotation(o.x, o.z, h)
     applyRotation(o.x, o.y, p)
