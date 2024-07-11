29e9830e7c8670c0648030202c1f64a7a6cfa56c
ops <p.schiel@gmail.com>
2024-07-11 21:02:17 +0200

    dbgsnip

--- snippets/debug.lua
+++ snippets/debug.lua
@@ -1,5 +1,3 @@
 package.cpath = package.cpath .. ';C:/Users/ops/.vscode/extensions/tangzx.emmylua-0.6.18/debugger/emmy/windows/x64/?.dll'
 local dbg = require('emmy_core')
 dbg.tcpConnect('localhost', 9966)
-
-dofile('E:/Work/dcs/opsdcs/snippets/inspect-globals.lua')
