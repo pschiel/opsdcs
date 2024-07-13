3e33046d09018bb1ab52dec9f5eb5bb8392becb1
pschiel <p.schiel@gmail.com>
2024-07-14 00:25:19 +0200

    events

--- snippets/debug.lua
+++ snippets/debug.lua
@@ -1,3 +1,5 @@
 package.cpath = package.cpath .. ';C:/Users/ops/.vscode/extensions/tangzx.emmylua-0.6.18/debugger/emmy/windows/x64/?.dll'
 local dbg = require('emmy_core')
 dbg.tcpConnect('localhost', 9966)
+
+dofile([[E:\Work\dcs\opsdcs-wip\scratch\todebug.lua]])
