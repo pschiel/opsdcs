edb9b39c3e103950f3a99ccaaad951d2f47a3ab8
pschiel <p.schiel@gmail.com>
2024-07-29 21:38:35 +0200

    sdf

--- snippets/switch-player-aircraft.lua
+++ snippets/switch-player-aircraft.lua
@@ -1,6 +1,6 @@
 local function switchAircraft()
     Group.getByName("g2"):activate()
-    net.dostring_in("gui", "DCS.setPlayerUnit(" .. Unit.getByName("p2") .. ")")
+    net.dostring_in("gui", "DCS.setPlayerUnit(" .. Unit.getByName("p2"):getID() .. ")")
 end
 
 missionCommands.addCommand("switch aircraft",  nil, switchAircraft)
