841bd599ba43346f30c6e9066c1978d727428f6b
pschiel <p.schiel@gmail.com>
2024-07-29 23:37:24 +0200

    refactor

--- snippets/switch-player-aircraft.lua
+++ snippets/switch-player-aircraft.lua
@@ -1,11 +1,24 @@
-local function switchAircraft()
-    Group.getByName("g2"):activate()
-    net.dostring_in("gui", "DCS.setPlayerUnit(" .. Unit.getByName("p2"):getID() .. ")")
+--- Switches SP player to a unit (can be in late activation)
+--- @param unitName string @unit name
+--- @param unpause boolean @if set to false, automatic unpause on briefing screen
+local function switchAircraft(unitName, unpause)
+    net.dostring_in("gui", "DCS.setPlayerUnit(" .. Unit.getByName(unitName):getID() .. ")")
+    if unpause then
+        net.dostring_in("gui", [[
+            local once = false
+            local hook = {
+                onSimulationPause = function()
+                    if once then return end
+                    DCS.setPause(false)
+                    once = true
+                end
+            }
+            DCS.setUserCallbacks(hook)
+        ]])
+    end
 end
 
-missionCommands.addCommand("switch aircraft",  nil, switchAircraft)
-
-net.dostring_in("gui", [[
-    local hook = { onSimulationPause = function() DCS.setPause(false); hook.onSimulationPause = nil end }
-    DCS.setUserCallbacks(hook)
-]])
+missionCommands.addCommand("switch to unit p1",  nil, switchAircraft, "p1", true)
+missionCommands.addCommand("switch to unit p2",  nil, switchAircraft, "p2", true)
+missionCommands.addCommand("switch to unit p3",  nil, switchAircraft, "p3", false)
+missionCommands.addCommand("switch to unit p4",  nil, switchAircraft, "p4", true)
