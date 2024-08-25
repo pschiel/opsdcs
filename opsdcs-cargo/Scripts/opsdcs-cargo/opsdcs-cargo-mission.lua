c10af7b13da0530a99fdbc303f70cefbad431e5d
ops <p.schiel@gmail.com>
2024-08-25 23:34:01 +0200

    wip

--- opsdcs-cargo/Scripts/opsdcs-cargo/opsdcs-cargo-mission.lua
+++ opsdcs-cargo/Scripts/opsdcs-cargo/opsdcs-cargo-mission.lua
@@ -11,105 +11,96 @@ OpsdcsCargo = {
 
     --- @type table<string,table> @static name to unit properties (unit type, static shape_name, category)
     staticToUnit = {
-        ["AAA_M1_37mm_cargo"] = { "M1_37mm" },
-        ["aaa_8_8cm_flak36_cargo"] = { "flak36" },
-        ["aaa_8_8cm_flak37_cargo"] = { "flak37" },
-        ["aaa_8_8cm_flak41_cargo"] = { "flak41" },
-        ["aaa_8_8cm_flak_18_cargo"] = { "flak18" },
-        ["aaa_bofors_40mm_cargo"] = { "bofors40" },
-        ["aaa_fire_can_son_9_cargo"] = { "SON_9" },
-        ["aaa_flak38_20mm_cargo"] = { "flak30" },
-        ["aaa_flakvierling38_quad_20mm_cargo"] = { "flak38" },
-        ["aaa_kdo.g.40_cargo"] = { "KDO_Mod40" },
-        ["aaa_ks_19_100mm_cargo"] = { "KS-19" },
-        ["aaa_m45_quadmount_hb_12.7mm_cargo"] = { "M45_Quadmount" },
-        ["aaa_qf_3_7_cargo"] = { "QF_37_AA" },
-        ["aaa_s_60_57mm_cargo"] = { "S-60_Type59_Artillery" },
-        ["aaa_zu23_ural4320_cargo"] = { "Ural-375 ZU-23" },
-        ["ah_1w_cargo"] = { "AH-1W" },
-        ["allies_rangefinder_drt_cargo"] = { "Allies_Director" },
-        ["apc_m2a1_halftrack_cargo"] = { "M2A1_halftrack" },
-        ["apc_sd_kfz251_halftrack_cargo"] = { "Sd_Kfz_251" },
-        ["arleigh_burke_iia_cargo"] = { "USS_Arleigh_Burke_IIa" },
-        ["atg_hmmwv_cargo"] = { "M1045 HMMWV TOW" },
-        ["brdm_2l1_arv_cargo"] = { "CH_BRDM2L1" },
-        ["bv_410_atv_cargo"] = { "BV410" },
-        ["car_daimler_armored_cargo"] = { "Daimler_AC" },
-        ["eagle_iv_mrap_cargo"] = { "CH_EagleIV" },
-        ["fh_lefh18_105mm_cargo"] = { "LeFH_18-40-105" },
-        ["fh_m2a1_105mm_cargo"] = { "M2A1-105" },
-        ["fh_pak40_75mm_cargo"] = { "Pak40" },
-        ["hkp_15b_luh_cargo"] = { "HKP15B" },
-        ["hq_7_ln_cargo"] = { "HQ-7_LN_P" },
-        ["hq_7_self_propelled_ln_cargo"] = { "HQ-7_LN_SP" },
-        ["hq_7_self_propelled_str_cargo"] = { "HQ-7_STR_SP" },
-        ["ka_52_cargo"] = { "CH_Ka52" },
-        ["kozak_5_apc_cargo"] = { "CH_Kozak5" },
-        ["kraz_spartan_apc_cargo"] = { "CH_KrAZSpartan" },
-        ["m777_cargo"] = { "CH_M777LTH_M795" },
-        ["m777_excalibur_cargo"] = { "CH_M777LTH_MTVR_M982" },
-        ["maschinensatz_33_gen_cargo"] = { "Maschinensatz_33" },
-        ["mlrs_hl_b8m1_80mm_cargo"] = { "HL_B8M1" },
-        ["mlrs_lc_b8m1_80mm_cargo"] = { "tt_B8M1" },
-        ["mortar_2b11_120mm_cargo"] = { "2B11 mortar" },
-        ["oh58d_cargo"] = { "OH58D" },
-        ["oshkosh_latv_m2_cargo"] = { "CH_OshkoshLATV_M2" },
-        ["oshkosh_latv_mk19_cargo"] = { "CH_OshkoshLATV_MK19" },
-        ["oshkosh_matv_mrap_m2_cargo"] = { "CH_OshkoshMATV_M2" },
-        ["oshkosh_matv_mrap_mk19_cargo"] = { "CH_OshkoshMATV_MK19" },
-        ["rbs_90_stationary_sam_ln_cargo"] = { "RBS-90" },
-        ["sa342l_cargo"] = { "SA342L" },
-        ["sa342m_cargo"] = { "SA342M" },
-        ["sa342minigun_cargo"] = { "SA342Minigun" },
-        ["sa342mistral_cargo"] = { "SA342Mistral" },
-        ["sam_avenger_stinger_cargo"] = { "M1097 Avenger" },
-        ["sam_chaparral_m48_cargo"] = { "M48 Chaparral" },
-        ["sam_hawk_cwar_an_mpq_55_cargo"] = { "Hawk cwar" },
-        ["sam_hawk_ln_m192_cargo"] = { "Hawk ln" },
-        ["sam_hawk_platoon_command_post_pcp_cargo"] = { "Hawk pcp" },
-        ["sam_hawk_sr_an_mpq_50_cargo"] = { "Hawk sr" },
-        ["sam_hawk_tr_an_mpq_46_cargo"] = { "Hawk tr" },
-        ["sam_nasams_c2_cargo"] = { "NASAMS_Command_Post" },
-        ["sam_nasams_ln_aim_120b_cargo"] = { "NASAMS_LN_B" },
-        ["sam_nasams_ln_aim_120c_cargo"] = { "NASAMS_LN_C" },
-        ["sam_nasams_sr_mpq64f1_cargo"] = { "NASAMS_Radar_MPQ64F1" },
-        ["sam_patriot_c2_icc_cargo"] = { "Patriot cp" },
-        ["sam_patriot_ecs_cargo"] = { "Patriot ECS" },
-        ["sam_rapier_blindfire_tr_cargo"] = { "rapier_fsa_blindfire_radar" },
-        ["sam_rapier_ln_cargo"] = { "rapier_fsa_launcher" },
-        ["sam_rapier_tracker_cargo"] = { "rapier_fsa_optical_tracker_unit" },
-        ["sam_sa_2_s_75_guideline_ln_cargo"] = { "S_75M_Volhov" },
-        ["sam_sa_2_s_75_rd_75_amazonka_rf_cargo"] = { "RD_75" },
-        ["sam_sa_9_strela_1_gaskin_tel_cargo"] = { "Strela-1 9P31" },
-        ["scout_bdrm2_cargo"] = { "BRDM2" },
-        ["scout_cobra_cargo"] = { "Cobra" },
-        ["scout_hl_dshk_cargo"] = { "HL_DSHK" },
-        ["scout_hl_kord_cargo"] = { "HL_KORD" },
-        ["scout_hmmwv_cargo"] = { "M1043" },
-        ["scout_lc_dshk_cargo"] = { "tt_DSHK" },
-        ["scout_lc_kord_cargo"] = { "tt_KORD" },
-        ["scout_m8_greyhound_ac_cargo"] = { "M8_Greyhound" },
-        ["scout_puma_ac_cargo"] = { "Sd_Kfz_234_2_Puma" },
-        ["skyshield_c_ram_gun_cargo"] = { "CH_Skyshield_Gun" },
-        ["sl_flakscheinwerfer_37_cargo"] = { "Flakscheinwerfer_37" },
-        ["spaaa_hl_with_zu23_cargo"] = { "HL_ZU-23" },
-        ["spaaa_lc_with_zu23_cargo"] = { "tt_ZU-23" },
-        ["sph_sd_kfz124_wespe_105mm_cargo"] = { "Wespe124" },
-        ["spm_2s9_nona_120mm_m_cargo"] = { "SAU 2-C9" },
-        ["tigr_m_cargo"] = { "CH_TigrM" },
-        ["tk_tetrarch_cargo"] = { "Tetrarch" },
-        ["tractor_m4_high_speed_cargo"] = { "M4_Tractor" },
-        ["uh_1h_cargo"] = { "UH-1H" },
-        ["volvo_740_ifv_cargo"] = { "Volvo740" },
-        ["wiesel_1a4_awc_cargo"] = { "CH_Wiesel1A4" },
-        ["wiesel_2_ozelot_vshorad_cargo"] = { "CH_Wiesel2Ozelot" },
-        ["bambi_bucket"] = { nil, "bambi_bucket", "Cargos" },
-        ["fueltank_cargo"] = { nil, "fueltank_cargo", "Cargos" },
+        ["oc_aaa_m1_37mm"] = { "M1_37mm" },
+        ["oc_aaa_8_8cm_flak36"] = { "flak36" },
+        ["oc_aaa_8_8cm_flak37"] = { "flak37" },
+        ["oc_aaa_8_8cm_flak41"] = { "flak41" },
+        ["oc_aaa_8_8cm_flak18"] = { "flak18" },
+        ["oc_aaa_bofors_40mm"] = { "bofors40" },
+        ["oc_aaa_fire_can_son_9"] = { "SON_9" },
+        ["oc_aaa_flak38_20mm"] = { "flak30" },
+        ["oc_aaa_flakvierling38_quad_20mm"] = { "flak38" },
+        ["oc_aaa_kdo_g_40"] = { "KDO_Mod40" },
+        ["oc_aaa_ks_19_100mm"] = { "KS-19" },
+        ["oc_aaa_m45_quadmount_hb_12_7mm"] = { "M45_Quadmount" },
+        ["oc_aaa_qf_3_7"] = { "QF_37_AA" },
+        ["oc_aaa_s_60_57mm"] = { "S-60_Type59_Artillery" },
+        ["oc_aaa_zu23_ural4320"] = { "Ural-375 ZU-23" },
+        ["oc_allies_rangefinder_drt"] = { "Allies_Director" },
+        ["oc_apc_m2a1_halftrack"] = { "M2A1_halftrack" },
+        ["oc_apc_sd_kfz251_halftrack"] = { "Sd_Kfz_251" },
+        ["oc_ch_usa_arleigh_burke_iia"] = { "USS_Arleigh_Burke_IIa" },
+        ["oc_atg_hmmwv"] = { "M1045 HMMWV TOW" },
+        ["oc_ch_ukr_brdm_2l1_arv"] = { "CH_BRDM2L1" },
+        ["oc_ch_swe_bv_410_atv"] = { "BV410" },
+        ["oc_car_daimler_armored"] = { "Daimler_AC" },
+        ["oc_ch_ger_eagle_iv_mrap"] = { "CH_EagleIV" },
+        ["oc_fh_lefh18_105mm"] = { "LeFH_18-40-105" },
+        ["oc_fh_m2a1_105mm"] = { "M2A1-105" },
+        ["oc_fh_pak40_75mm"] = { "Pak40" },
+        ["oc_hq_7_ln"] = { "HQ-7_LN_P" },
+        ["oc_hq_7_self_propelled_ln"] = { "HQ-7_LN_SP" },
+        ["oc_hq_7_self_propelled_str"] = { "HQ-7_STR_SP" },
+        ["oc_ch_ukr_kozak_5_apc"] = { "CH_Kozak5" },
+        ["oc_ch_ukr_kraz_spartan_apc"] = { "CH_KrAZSpartan" },
+        ["oc_ch_usa_m777"] = { "CH_M777LTH_M795" },
+        ["oc_ch_usa_m777_excalibur"] = { "CH_M777LTH_MTVR_M982" },
+        ["oc_maschinensatz_33_gen"] = { "Maschinensatz_33" },
+        ["oc_mlrs_hl_b8m1_80mm"] = { "HL_B8M1" },
+        ["oc_mlrs_lc_b8m1_80mm"] = { "tt_B8M1" },
+        ["oc_mortar_2b11_120mm"] = { "2B11 mortar" },
+        ["oc_ch_usa_oshkosh_latv_m2"] = { "CH_OshkoshLATV_M2" },
+        ["oc_ch_usa_oshkosh_latv_mk19"] = { "CH_OshkoshLATV_MK19" },
+        ["oc_ch_usa_oshkosh_matv_mrap_m2"] = { "CH_OshkoshMATV_M2" },
+        ["oc_ch_usa_oshkosh_matv_mrap_mk19"] = { "CH_OshkoshMATV_MK19" },
+        ["oc_ch_swe_rbs_90_stationary_sam_ln"] = { "RBS-90" },
+        ["oc_sam_avenger_stinger"] = { "M1097 Avenger" },
+        ["oc_sam_chaparral_m48"] = { "M48 Chaparral" },
+        ["oc_sam_hawk_cwar_an_mpq_55"] = { "Hawk cwar" },
+        ["oc_sam_hawk_ln_m192"] = { "Hawk ln" },
+        ["oc_sam_hawk_platoon_command_post_pcp"] = { "Hawk pcp" },
+        ["oc_sam_hawk_sr_an_mpq_50"] = { "Hawk sr" },
+        ["oc_sam_hawk_tr_an_mpq_46"] = { "Hawk tr" },
+        ["oc_sam_nasams_c2"] = { "NASAMS_Command_Post" },
+        ["oc_sam_nasams_ln_aim_120b"] = { "NASAMS_LN_B" },
+        ["oc_sam_nasams_ln_aim_120c"] = { "NASAMS_LN_C" },
+        ["oc_sam_nasams_sr_mpq64f1"] = { "NASAMS_Radar_MPQ64F1" },
+        ["oc_sam_patriot_c2_icc"] = { "Patriot cp" },
+        ["oc_sam_patriot_ecs"] = { "Patriot ECS" },
+        ["oc_sam_rapier_blindfire_tr"] = { "rapier_fsa_blindfire_radar" },
+        ["oc_sam_rapier_ln"] = { "rapier_fsa_launcher" },
+        ["oc_sam_rapier_tracker"] = { "rapier_fsa_optical_tracker_unit" },
+        ["oc_sam_sa_2_s_75_guideline_ln"] = { "S_75M_Volhov" },
+        ["oc_sam_sa_2_s_75_rd_75_amazonka_rf"] = { "RD_75" },
+        ["oc_sam_sa_9_strela_1_gaskin_tel"] = { "Strela-1 9P31" },
+        ["oc_scout_bdrm2"] = { "BRDM2" },
+        ["oc_scout_cobra"] = { "Cobra" },
+        ["oc_scout_hl_dshk"] = { "HL_DSHK" },
+        ["oc_scout_hl_kord"] = { "HL_KORD" },
+        ["oc_scout_hmmwv"] = { "M1043" },
+        ["oc_scout_lc_dshk"] = { "tt_DSHK" },
+        ["oc_scout_lc_kord"] = { "tt_KORD" },
+        ["oc_scout_m8_greyhound_ac"] = { "M8_Greyhound" },
+        ["oc_scout_puma_ac"] = { "Sd_Kfz_234_2_Puma" },
+        ["oc_ch_ger_skyshield_c_ram_gun"] = { "CH_Skyshield_Gun" },
+        ["oc_sl_flakscheinwerfer_37"] = { "Flakscheinwerfer_37" },
+        ["oc_spaaa_hl_with_zu23"] = { "HL_ZU-23" },
+        ["oc_spaaa_lc_with_zu23"] = { "tt_ZU-23" },
+        ["oc_sph_sd_kfz124_wespe_105mm"] = { "Wespe124" },
+        ["oc_spm_2s9_nona_120mm_m"] = { "SAU 2-C9" },
+        ["oc_ch_rus_tigr_m"] = { "CH_TigrM" },
+        ["oc_tk_tetrarch"] = { "Tetrarch" },
+        ["oc_tractor_m4_high_speed"] = { "M4_Tractor" },
+        ["oc_ch_swe_volvo_740_ifv"] = { "Volvo740" },
+        ["oc_ch_ger_wiesel_1a4_awc"] = { "CH_Wiesel1A4" },
+        ["oc_ch_ger_wiesel_2_ozelot_vshorad"] = { "CH_Wiesel2Ozelot" },
+        ["oc_bambi_bucket"] = { nil, "bambi_bucket", "Cargos" },
+        ["oc_fueltank_cargo"] = { nil, "fueltank_cargo", "Cargos" },
     },
 
     basedir = OpsdcsCargoBasedir or "", --- @type string @path to Scripts/opsdcs-cargo
     isRunning = false,                  --- @type boolean @true while running
-    trackedStatics = {},   
+    trackedStatics = {},
 }
 
 ------------------------------------------------------------------------------
@@ -165,7 +156,7 @@ function OpsdcsCargo:convertStaticToUnit(name)
         ["route"] = {},
         ["tasks"] = {},
         ["hidden"] = false,
-        ["units"] ={
+        ["units"] = {
             [1] = {
                 ["name"] = name,
                 ["type"] = type,
@@ -270,19 +261,20 @@ function OpsdcsCargo:update()
             end
         else
             local pos = s:getPosition()
-			local heading = math.atan2(pos.x.z, pos.x.x)
+            local heading = math.atan2(pos.x.z, pos.x.x)
             -- save last known position in case of loss
             data.x = pos.p.x
             data.y = pos.p.z
             data.heading = heading
+            local deg = math.deg(heading)
             local agl = pos.p.y - land.getHeight({ x = pos.p.x, y = pos.p.z })
             local v_y = s:getVelocity().y
-            msg = msg .. string.format(" AGL=%.1f, v_y=%.5f, hdg:%.2f (%s)", agl, v_y, heading, self.trackedStatics[name].type)
+            msg = msg .. string.format(" AGL=%.1f, v_y=%.5f, hdg:%.2f (%s)", agl, v_y, deg, self.trackedStatics[name].type)
             -- transport state: needs to be in air first
             if data.state == "init" and agl > 1 and v_y > 0 then
                 --self:log(name .. " is in transport")
                 data.state = "transport"
-            -- dropped: when in transport and doesn't move anymore or glitched in ground
+                -- dropped: when in transport and doesn't move anymore or glitched in ground
             elseif data.type == "fueltank_cargo" and data.state == "transport" and (v_y < -1 and agl < 1) then
                 data.state = "dropped"
                 trigger.action.explosion(s:getPosition().p, 100)
@@ -291,7 +283,7 @@ function OpsdcsCargo:update()
                 data.state = "dropped"
                 s:destroy()
                 self:convertStaticToUnit(name)
-            -- lost before going transport
+                -- lost before going transport
             elseif data.state == "init" and agl < -20 then
                 --self:log(name .. " was dropped")
                 data.state = "dropped"
@@ -301,7 +293,7 @@ function OpsdcsCargo:update()
         end
     end
 
-    --trigger.action.outText(msg, 1, true)
+    trigger.action.outText(msg, 1, true)
 
     timer.scheduleFunction(self.update, self, timer.getTime() + timeDelta)
 end
