559c557e7fa0ec2c9662a442eda6469302ec520e
ops <p.schiel@gmail.com>
2025-02-22 04:07:52 +0100

    sdfg

--- examples/Scripts/Hooks/lua-imgui-example/ImGui.lua
+++ examples/Scripts/Hooks/lua-imgui-example/ImGui.lua
@@ -1,13 +1,39 @@
-lua_imgui_path = lua_imgui_path or (package.cpath..";"..LockOn_Options.script_path.."LuaImGui\\?.dll")
+-- MIT License
+
+-- Copyright (c) 2024 Joshua Nelson
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+lua_imgui_dll_path = lua_imgui_dll_path or LockOn_Options.script_path.."LuaImGui"
+lua_imgui_lua_path = lua_imgui_lua_path or LockOn_Options.script_path .. "LuaImGui"
 
 function require_imgui()
-  package.cpath = package.cpath..";"..lua_imgui_path
-  success,result = pcall(require,'LuaImGui')
-  return result
+    package.cpath = package.cpath .. ";" .. lua_imgui_dll_path .. "\\?.dll"
+    success, result = pcall(require, 'LuaImGui')
+    if not success then
+        print_message_to_user("LuaImGui failed to load dll - See Log")
+    end
+    return result
 end
 
 function require_imgui_stubs()
-    package.cpath = package.cpath..";"..lua_imgui_path
+    package.path = package.path .. ";" .. lua_imgui_lua_path .. "\\?.lua"
     return require("ImGuiStubs")
 end
 
@@ -17,8 +43,6 @@ if ( imgui_disabled ) then
 else
     ImGui = require_imgui()
 end
-
-ImGui = require_imgui()
 
 function ImGui:Window(name, f)
     self:Begin(name)
@@ -74,9 +98,19 @@ function ImGui:Table(t)
     self:Columns(1)
 end
 
+function ImGui:Plot(plot_name, x_axis_name, y_axis_name, width, f)
+    ImGui:BeginPlot(plot_name, x_axis_name, y_axis_name, width)
+    f()
+    ImGui:EndPlot()
+end
+
 
 function ImGui.Serialize(t, depth, seen)
     
+    if t == ImGui then
+        error("Did you call ImGui:Serialize? Should be ImGui.Serialize")
+    end
+
     if t == nil then
         return "nil"
     end
@@ -100,14 +134,31 @@ function ImGui.Serialize(t, depth, seen)
 
     local output = "{\n"
 
-    local strings = {}
-    for i,v in pairs(t) do
+    local i_strings = {}
+    for i, v in ipairs(t) do
         if type(v) == 'table' then
-            table.insert(strings, next_space..i.." = "..ImGui.Serialize(v, depth + 1, seen))
+            table.insert(i_strings, next_space..ImGui.Serialize(v, depth + 1, seen))
         elseif type(v) == 'string' then
-            table.insert(strings, next_space..i..string.format(" = \"%s\"", v))
+            table.insert(i_strings, next_space..i..string.format("\"%s\"", v))
         else
-            table.insert(strings, next_space..i.." = "..tostring(v))
+            table.insert(i_strings, next_space..tostring(v))
+        end
+    end
+
+    
+
+
+    local strings = {}
+    for i, v in pairs(t) do
+
+        if i_strings[i] == nil then -- check it hasn't been included in i_strings already
+            if type(v) == 'table' then
+                table.insert(strings, next_space..i.." = "..ImGui.Serialize(v, depth + 1, seen))
+            elseif type(v) == 'string' then
+                table.insert(strings, next_space..i..string.format(" = \"%s\"", v))
+            else
+                table.insert(strings, next_space..i.." = "..tostring(v))
+            end
         end
     end
 
@@ -115,15 +166,14 @@ function ImGui.Serialize(t, depth, seen)
     if mt ~= nil then
         table.insert( strings, next_space..string.format("metatable(%s) = ", tostring(getmetatable(t)))..ImGui.Serialize(getmetatable(t), depth + 1, seen))
     end
-    -- for i,v in pairs(getmetatable(t)) do
-    --     if type(v) == 'table' then
-    --         table.insert(strings, i.." = "..next_space..Serialize(t, depth + 1, seen))
-    --     else
-    --         table.insert(strings, i.." = "..next_space..tostring(v))
-    --     end
-    -- end
 
-    output = output..table.concat(strings, ',\n')..'\n'
+    strings = table.concat(strings, ',\n')
+    
+    if #i_strings > 0 then
+        i_strings = table.concat(i_strings, ',\n')
+        strings = table.concat({i_strings, strings}, ',\n')
+    end
+    output = output..strings..'\n'
 
     output = output..space.."}"
     return output
--- examples/Scripts/Hooks/lua-imgui-example/README.md
+++ examples/Scripts/Hooks/lua-imgui-example/README.md
@@ -1,1 +1,6 @@
 see https://github.com/08jne01/dcs-lua-imgui
+
+Installation:
+
+1. Copy this file to Scripts/Hooks folder
+2. Copy LuaImGui folder to Scripts folder
