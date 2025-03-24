fc3146905c59b64feac658e673a88348c54d4dd0
ops <p.schiel@gmail.com>
2025-03-24 22:45:10 +0100

    adf

--- snippets/debug.lua
+++ snippets/debug.lua
@@ -1,8 +1,9 @@
 -- debug snippet
 
-package.cpath = package.cpath .. ';C:/Users/ops/.vscode/extensions/tangzx.emmylua-0.8.18-win32-x64/debugger/emmy/windows/x64/?.dll'
-local dbg = require('emmy_core')
-dbg.tcpConnect('localhost', 9966)
+pcall(function()
+    package.cpath = package.cpath .. ';C:/Users/pschi/.vscode/extensions/tangzx.emmylua-0.8.20-win32-x64/debugger/emmy/windows/x64/?.dll'
+    local dbg = require('emmy_core')
+    dbg.tcpConnect('localhost', 9966)
+end)
 
---dofile([[E:\Work\dcs\opsdcs\snippets\modify-next-mission.lua]])
-dofile([[E:\Work\dcs\opsdcs\scratch\todebug.lua]])
+--dofile([[F:\Work\dcs\opsdcs\scratch\todebug.lua]])
